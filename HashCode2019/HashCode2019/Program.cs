44cd4461845fc6923e722d243b33d038ae4fcf2d
Dimas <dimasdelafuen@gmail.com>
2019-02-27 21:51:20 +0100

    Project init

---
+++ HashCode2019/HashCode2019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HashCode2019
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static object Load(string file)
+        {
+            string filename = $"Inputs/{file}.in";
+            string[] fileContent = File.ReadAllLines(filename);
+            int[] parameters = fileContent[0].Split(' ').Select((s) => int.Parse(s)).ToArray();
+
+            int rows = parameters[0];
+            int columns = parameters[1];
+
+           //Read file content
+
+            return null;
+        }
+
+
+        public static void Save(string file, object o)
+        {
+            string filename = $"Outputs/{file}.out";
+            List<string> content = new List<string>();
+            //Get object properties
+
+            File.WriteAllLines(filename, content.ToArray());
+        }
+    }
+}

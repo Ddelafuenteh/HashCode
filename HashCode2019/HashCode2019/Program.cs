eed9be08ab39eb6eb57f6b1912c3024d5c6a199e
Juan Manuel Pedraza <jackpanzer2012@gmail.com>
2019-02-28 18:35:34 +0100

    Beautified load method?

--- HashCode2019/HashCode2019/Program.cs
+++ HashCode2019/HashCode2019/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace HashCode2019
 {
@@ -21,16 +22,43 @@ namespace HashCode2019
             }
         }
 
-        static object Load(string file)
+        static object Load(string filePath)
         {
-            string filename = $"Inputs/{file}.in";
-            string[] fileContent = File.ReadAllLines(filename);
-            int[] parameters = fileContent[0].Split(' ').Select((s) => int.Parse(s)).ToArray();
+            // string filename = $"Inputs/{file}.in";
+            // string[] fileContent = File.ReadAllLines(filename);
+            // int[] parameters = fileContent[0].Split(' ').Select((s) => int.Parse(s)).ToArray();
 
-            int rows = parameters[0];
-            int columns = parameters[1];
+            // int rows = parameters[0];
+            // int columns = parameters[1];
 
-           //Read file content
+            ////Read file content
+
+            // return null;
+
+            //var toLoad = new Pizza();
+
+            using (var file = new StreamReader($"Inputs/{ filePath }", Encoding.Default))
+            {
+                var firstInput = file.ReadLine().Split(' ').Select(val => int.Parse(val)).ToArray();
+
+                // Header information
+                //toLoad.Information = new PizzaInformation()
+                //{
+                //    TotalAmountOfRows = firstInput[0],
+                //    TotalAmountOfColumns = firstInput[1],
+                //    MinimumAmountPerIngredient = firstInput[2],
+                //    MaximumIngredientsInTotal = firstInput[3]
+                //};
+
+                //toLoad.Deliciousness = new char[firstInput[0]][];
+                var currentIndex = 0;
+
+                while (!file.EndOfStream)
+                {
+                    var content = file.ReadLine();
+                    //toLoad.Deliciousness[currentIndex++] = file.ReadLine().ToCharArray();
+                }
+            }
 
             return null;
         }

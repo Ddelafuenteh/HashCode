5553bbee66c961798d3d997b80971d8754b3ee28
Juan Manuel Pedraza <jackpanzer2012@gmail.com>
2019-02-28 20:27:54 +0100

    Coincidences and matches

---
+++ HashCode2019/HashCode2019/Coincidence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2019
+{
+    public class Coincidence
+    {
+        public string Tag { get; set; }
+        public IList<Match> Matches { get; set; }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
+
+            if (!(obj is Coincidence))
+                return false;
+
+            return Tag.Equals((obj as Coincidence).Tag);
+        }
+    }
+
+    public class Match
+    {
+        public int PhotoId { get; set; }
+        public int NumberOfCoincidences { get; set; }
+    }
+}
--- HashCode2019/HashCode2019/Program.cs
+++ HashCode2019/HashCode2019/Program.cs
@@ -14,7 +14,7 @@ namespace HashCode2019
             foreach (var file in files)
             {
                 Console.WriteLine($"Processing {file}");
-                var model = Load(file);
+                var model = Load(file).Item1;
                 //model.Run();
                 //PUNTOS++
                 //prueba
@@ -22,11 +22,13 @@ namespace HashCode2019
             }
         }
 
-        static IList<Photo> Load(string filePath)
+        static (IList<Photo>, HashSet<Coincidence>) Load(string filePath)
         {
             var toRet = new List<Photo>();
             string filename = $"./../../../Inputs/{filePath}.txt";
             var currentPhotoId = 0;
+
+            var coincidences = new HashSet<Coincidence>();
 
             using (var file = new StreamReader(filename, Encoding.Default))
             {
@@ -35,18 +37,41 @@ namespace HashCode2019
                 while (!file.EndOfStream)
                 {
                     var content = file.ReadLine().Split(' ');
-                    string[] leftover = new string[content.Length - 2];
-                    Array.Copy(content, 2, leftover, 0, int.Parse(content[1]));
+                    string[] tagArray = new string[int.Parse(content[1])];
+                    Array.Copy(content, 2, tagArray, 0, int.Parse(content[1]));
 
                     toRet.Add(new Photo() {
                         Orientation = content[0].ElementAt(0) == 'H' ? Orientation.Horizontal : Orientation.Vertical,
-                        ID = currentPhotoId++,
-                        Tags = new HashSet<string>(leftover)
+                        ID = currentPhotoId,
+                        Tags = new HashSet<string>(tagArray)
                     });
+
+                    foreach(var currentTag in tagArray)
+                    {
+                        var coincidence = coincidences.FirstOrDefault(val => val.Tag.Equals(currentTag));
+
+                        if (coincidence == null)
+                        {
+                            coincidence = new Coincidence()
+                            {
+                                Tag = currentTag,
+                                Matches = new List<Match>()
+                            };
+
+                            coincidences.Add(coincidence);
+                        }
+
+                        coincidence.Matches.Add(new Match() {
+                            PhotoId = currentPhotoId,
+                            NumberOfCoincidences = tagArray.Length
+                        });
+                    }
+
+                    currentPhotoId++;
                 }
             }
 
-            return toRet.OrderByDescending(x => x.Tags.Count).ToList();
+            return (toRet.OrderByDescending(x => x.Tags.Count).ToList(), coincidences);
         }
 
 

1dbc3d58b53cf0f09d5a29dc43024731a4b9e651
cristian.gutierrez <cristiangn.developer@hotmail.com>
2019-02-28 19:10:33 +0100

    Models

---
+++ HashCode2019/HashCode2019/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2019
+{
+    public class Photo
+    {
+        public  int ID { get; set; }
+        public Horientacion Horientation { get; set; }
+        public HashSet<string> Tags { get; set; }
+    }
+
+
+    public enum Horientacion
+    {
+        Vertical,
+        Horizontal
+    }
+}
---
+++ HashCode2019/HashCode2019/Slide.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2019
+{
+   public class Slide
+    {
+        public List<Photo> Photos { get; set; }
+
+
+        public Slide(Photo photo)
+        {
+            Photos = new List<Photo>
+            {
+                photo
+            };
+        }
+        public Slide(Photo photo1, Photo photo2)
+        {
+            Photos = new List<Photo>
+            {
+                photo1,
+                photo2
+            };
+        }
+    }
+}
